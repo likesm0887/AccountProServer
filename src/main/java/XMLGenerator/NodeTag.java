95e0a1b70b430cdc0b5bda5c9b8abc01f103e6c3
Jammy Tan <jammy1129@gmail.com>
2020-12-27 16:57:57 +0800

    add NodeTag

---
+++ src/main/java/XMLGenerator/NodeTag.java
@@ -0,0 +1,26 @@
+package XMLGenerator;
+
+public class NodeTag {
+    static final String CompanyRegistrationNumber = "ssmt-dei:CompanyRegistrationNumber";
+    static final String NameOfReportingEntity = "ssmt-dei:NameOfReportingEntity";
+    static final String OriginOfCompany = "ssmt-dei:OriginOfCompany";
+    static final String StatusOfCompany = "ssmt-dei:StatusOfCompany";
+    static final String TypeOfCompany = "ssmt-dei:TypeOfCompany";
+    static final String DisclosureOfFinancialStatementsPreparationForCurrentSubmission = "ssmt-dei:DisclosureOfFinancialStatementsPreparationForCurrentSubmission";
+    static final String CompanyCurrentFinancialYearStartDate = "ssmt-dei:CompanyCurrentFinancialYearStartDate";
+    static final String CompanyCurrentFinancialYearEndDate = "ssmt-dei:CompanyCurrentFinancialYearEndDate";
+    static final String CompanyPreviousFinancialYearStartDate = "ssmt-dei:CompanyPreviousFinancialYearStartDate";
+    static final String CompanyPreviousFinancialYearEndDate = "ssmt-dei:CompanyPreviousFinancialYearEndDate";
+    static final String StatusOfCarryingOnBusinessDuringFinancialYear = "ssmt-dei:StatusOfCarryingOnBusinessDuringFinancialYear";
+    static final String DisclosureOfFinancialStatementsAuditStatus = "ssmt-dei:DisclosureOfFinancialStatementsAuditStatus";
+    static final String BasisOfAccountingStandardsAppliedToPrepareFinancialStatements = "ssmt-dei:BasisOfAccountingStandardsAppliedToPrepareFinancialStatements";
+    static final String TypeOfSubmission = "ssmt-dei:TypeOfSubmission";
+    static final String NatureOfFinancialStatements = "ssmt-dei-ee-mfrs:NatureOfFinancialStatements"; // mfrs
+    static final String DescriptionOfPresentationCurrency = "ssmt-dei:DescriptionOfPresentationCurrency";
+    static final String DescriptionOfFunctionalCurrency = "ssmt-dei:DescriptionOfFunctionalCurrency";
+    static final String LevelOfRoundingUsedInFinancialStatements = "ssmt-dei:LevelOfRoundingUsedInFinancialStatements";
+    static final String DateOfFinancialStatementsApprovedByBoardOfDirectors = "ssmt:DateOfFinancialStatementsApprovedByBoardOfDirectors";
+    static final String DateOfCirculationOfFinancialStatementsAndReportsToMembers = "ssmt:DateOfCirculationOfFinancialStatementsAndReportsToMembers";
+    static final String DateOfFinancialStatementsAndReportsOfDirectorsAndAuditorsLaidInAnnualGeneralMeeting = "ssmt:DateOfFinancialStatementsAndReportsOfDirectorsAndAuditorsLaidInAnnualGeneralMeeting";
+    static final String DateOfStatutoryDeclaration = "ssmt:DateOfStatutoryDeclaration";
+}
